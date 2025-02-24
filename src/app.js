849bc72987194b81463b980b13fecc2043e2363c
Victor Terra <victorbterra@gmail.com>
2025-02-24 13:47:54 -0300

    feature: addind cors params

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import weatherRoutes from './routes/weather.js';
+import express from "express";
+import dotenv from "dotenv";
+import weatherRoutes from "./routes/weather.js";
+import cors from "cors";
 
 // CARREGAR VARIAVEIS DE AMBIENTE
 dotenv.config();
@@ -8,13 +9,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// HABILITAR CORS
+app.use(cors({
+  origin: "*", // permitir todos os domínios
+  credentials: true, // permitir que os cookies sejam transmitidos
+  methods: ["GET"], // permitir apenas GET
+  allowedHeaders: ["Content-Type", "Authorization"], // permitir os cabeçalhos Content-Type e Authorization
+ 
+}));
+
+
 // CONVERSÃO PARA JSON
 app.use(express.json());
 
-
 // ROTAS
 
-app.use('/api/weather', weatherRoutes);
+app.use("/api/weather", weatherRoutes);
 
 // INICIAR O SERVIDOR
 
